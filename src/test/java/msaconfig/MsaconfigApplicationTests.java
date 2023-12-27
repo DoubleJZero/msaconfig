3d71ddaea241f4c0afda239dcf869c215f978859
JandB <blueseajung@naver.com>
2023-12-27 11:17:19 +0900

    first

---
+++ src/test/java/msaconfig/MsaconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package msaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
